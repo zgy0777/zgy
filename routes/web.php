1ca7184afcd6fd83cb171aed7812c53eff92c36a
zgy0777 <soldier76@qq.com>
2018-09-03 11:12:58 +0000

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//主页
+Route::get('/', 'StaticPagesController@home')->name('home');
+//帮助页
+Route::get('/help', 'StaticPagesController@help')->name('help');
+//关于页
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+
